3ff698b9951352b4877793c01017429a84d57d72
siwill22 <siwill22@gmail.com>
2021-08-05 19:42:57 +0800

    add functions for creating deformation markers

--- gprm/utils/sphere.py
+++ gprm/utils/sphere.py
@@ -177,3 +177,46 @@ def groupby_healpix(df, equal_area_points, return_point_indices=True):
         return grouped_points
 
 
+def create_circles(centre_points, circle_radius_degrees=0.5, polygon=None):
+    # create small circles centred on the provide points, with user-specified radii
+    # optionally, limit the points to those which lie within a provided polygon geometry
+    # requires pyshtools
+    import pyshtools as pysh
+
+    if polygon:
+        pts_in_polygon = []
+        for pt in centre_points.multipoint.get_points():
+            if geom.is_point_in_polygon(pt):
+                pts_in_polygon.append(pt.to_lat_lon())
+        #print(pts_in_polygon[0])
+    
+        circle_list = []
+        for lat,lon in pts_in_polygon:
+            circle_list.append(pysh.utils.MakeCircleCoord(lat,lon,circle_radius_degrees))
+            
+    else:
+        circle_list = []
+        for lon,lat in zip(centre_points.longitude, centre_points.latitude):
+            circle_list.append(pysh.utils.MakeCircleCoord(lat,lon,circle_radius_degrees))
+
+    return circle_list
+
+    
+def create_graticule(graticule_spacing=1, xlims=[-180,180], ylims=[-90,90], along_line_point_density=0.1, polygon=None):
+    # create a latitude-longitude graticule, in the form of line geometries along each meridian and parallel 
+    # optionally specify the graticule spacing, extent on the globe, sampling of the vertices along each line
+    # optionally clip the graticule to a user-provided polygon geometry
+    graticule_lines = []
+    for xlevel in np.arange(xlims[0], xlims[1]+graticule_spacing, graticule_spacing):
+        graticule_lines.append(np.array([(ylevel,xlevel) for ylevel in np.arange(ylims[0], ylims[1]+along_line_point_density, along_line_point_density)]))
+        
+    for ylevel in np.arange(ylims[0], ylims[1]+graticule_spacing, graticule_spacing):
+        graticule_lines.append(np.array([(ylevel,xlevel) for xlevel in np.arange(xlims[0], xlims[1]+along_line_point_density, along_line_point_density)]))
+
+    if polygon:
+        graticule_lines_gdf = geometries_to_geodataframe(graticule_lines, geometry_type='PolyLine')
+        polygon_gdf = geometries_to_geodataframe([polygon.to_lat_lon_array()])
+        graticule_lines_in_polygon_gdf = gpd.overlay(graticule_lines_gdf, polygon_gdf, how='intersection', keep_geom_type=False)
+        graticule_lines = dataframe_to_geometries(graticule_lines_in_polygon_gdf)
+
+    return graticule_lines
